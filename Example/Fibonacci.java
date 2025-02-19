b4e16a5a60b182b355211e17310a5b21315e40e2
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2025-02-19 23:43:06 +0300

    Update Fibonacci.java

--- Example/Fibonacci.java
+++ Example/Fibonacci.java
@@ -1,29 +1,28 @@
 /*
 EXAMPLE
 Fibonacci serisi üretme
-Kullanıcının girdiği n kadar Fibonacci serisi üreten Java programı
+Kullanıcının girdiği bir sayı kadar elemanı olan Fibonacci serisini üreten Java programı
 */
 import java.util.Scanner;
 class Main {
     public static void main(String [] args) {
         Scanner s1 = new Scanner(System.in);
         Scanner s2 = new Scanner(System.in);
-        int n = 0, n1 = 0, n2 = 1, n3 = 0;
+        int n = 0;
+        long n1 = 0, n2 = 1, n3 = 0;
         String exit = "a";
         while (!exit.equals("e")) {
             n = 0; n1 = 0; n2 = 1; n3 = 0;
-            String fibonaccis = "" + n1 + " - " + n2 + "";
+            String fibonacciSerie = "";
             System.out.println("Fibonacci serisi kac elemanli olsun?");
             n = Math.abs(s1.nextInt());
-            for(int i = 0; i < (n-2); i++) {
-                if (n2 != n) {
-                    n3 = n1 + n2;
-                    n1 = n2;
-                    n2 = n3;
-                    fibonaccis += " - " + n3 + "";
-                }
+            for (int i = 0; i < n; i++) {
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                fibonacciSerie += " - " + n3 + " - ";
             }
-            System.out.println(fibonaccis);
+            System.out.println(fibonacciSerie);
             System.out.println("Kapatmak icin e'ye devam etmek icin herhangi bir tusa basiniz.");
             exit = s2.nextLine();
         }
